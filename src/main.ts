a306757bb651036b11f9e4bc7571f0fec5c674b4
FQDev <fquouhadiho@gmail.com>
2022-10-16 19:11:23 +0000

    log removed

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,17 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  window.console.log = function () {
+  };
+
+  window.console.info = function () {
+  };
+
+  window.console.warn = function () {
+  };
+
+  window.console.error = function () {
+  };
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
